7273f4b7b3c1ad2db79625a0ff7bb964a0abd8b0
PeterCzy <31866285+PeterCzy@users.noreply.github.com>
2019-07-30 00:48:41 -0400

    Update 39. Combination Sum.c

--- medium/39. Combination Sum.c
+++ medium/39. Combination Sum.c
@@ -3,42 +3,46 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-
-/* not accepted*/
-
 int cmpfunc(const void* a, const void* b){
     return *(int*)a - *(int*)b;
 }
 
-void generate(int* nums, int size, int target, int* returnSize, int** returnColumnSizes, int** ret, int* temp, int index){
+void helper(int* nums, int numsSize, int target, int* returnSize, int** returnColumnSizes, int** ret, int* temp, int index){
     if(target < 0)
         return;
     
-    if(target == 0){
-        ret[*returnSize] = (int*)malloc(index * sizeof(int));
-        memcpy(ret[*returnSize], temp, index);
-        returnColumnSizes[*returnSize] = (int*)malloc(sizeof(int));
-        returnColumnSizes[*returnSize][0] = index;
+    else if(target == 0){
+        ret[*returnSize] = (int*)malloc(index*sizeof(int));
+        memcpy(ret[*returnSize], temp, index*sizeof(int));
+        (*returnColumnSizes)[*returnSize] = index;
         (*returnSize)++;
         return;
     }
     
-    for(int i = 0; i < size; i++){
-        if(nums[i] > target) continue;
-        if(nums[i] >= temp[index - 1]){
-            temp[index] = nums[i];
-            generate(nums, size, target - nums[i], returnSize, returnColumnSizes, ret, temp, index + 1);
+    else{
+        for(int i = 0; i < numsSize; i++){
+            if(nums[i] > target)
+                continue;
+            else{
+                if(index > 0 && nums[i] < temp[index - 1])
+                    continue;
+                
+                temp[index] = nums[i];
+                helper(nums, numsSize, target - nums[i], returnSize, returnColumnSizes, ret, temp, index + 1);
+            }
         }
     }
 }
 
+
 int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
     qsort(candidates, candidatesSize, sizeof(int), cmpfunc);
-    int** ret = (int**)malloc(sizeof(int*));
-    int* temp = (int*)malloc(target * sizeof(int));
-    *returnColumnSizes = (int*)malloc(500 * sizeof(int));
+    int** ret = (int**)malloc(10000 * sizeof(int*));
+    int* temp = (int*)calloc(target + 1, sizeof(int));
     *returnSize = 0;
+    (*returnColumnSizes) = (int*)malloc(10000 * sizeof(int));
+
+    helper(candidates, candidatesSize, target, returnSize, returnColumnSizes, ret, temp, 0);
     
-    generate(candidates, candidatesSize, target, returnSize, **returnColumnSizes, ret, temp, 0);
     return ret;
 }
