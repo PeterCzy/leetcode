25b5d37d24439994d35d6920df6cdf04b01069da
PeterCzy <31866285+PeterCzy@users.noreply.github.com>
2019-07-30 20:44:06 -0400

    Create 47. Permutations II.c

---
+++ medium/47. Permutations II.c
@@ -0,0 +1,57 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+int cmpfunc(const void* a, const void* b){
+    return *(int*)a - *(int*)b;
+}
+
+void helper(int* nums, int numsSize, int* returnSize, int** returnColumnSizes, int*** ret, int* temp, int* table, int index){
+    if(index == numsSize){
+        (*ret) = (int**)realloc((*ret), (*returnSize + 1)*sizeof(int*));
+        (*ret)[*returnSize] = (int*)malloc(numsSize*sizeof(int));
+        memcpy((*ret)[*returnSize], temp, numsSize*sizeof(int));
+        
+        (*returnColumnSizes) = (int*)realloc((*returnColumnSizes), (*returnSize + 1)*sizeof(int));
+        (*returnColumnSizes)[*returnSize] = numsSize;
+        
+        (*returnSize)++;
+        
+        return;
+    }
+    else{
+        for(int i = 0; i < numsSize; i++){
+            if(i > 0 && nums[i] == nums[i - 1] && !table[i - 1])
+                continue;
+            
+            if(table[i] == 0){
+                temp[index] = nums[i];
+                table[i] = 1;
+                helper(nums, numsSize, returnSize, returnColumnSizes, ret, temp, table, index + 1);
+                
+                table[i] = 0;
+            }  
+        }
+    }
+}
+
+
+int** permuteUnique(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+    qsort(nums, numsSize, sizeof(int), cmpfunc);
+    int** ret = (int**)malloc(sizeof(int*));
+    (*returnColumnSizes) = (int*)malloc(sizeof(int));
+    
+    int* temp = (int*)malloc(numsSize*sizeof(int));
+    
+    int* table = (int*)malloc(numsSize*sizeof(int));
+    memset(table, 0, numsSize*sizeof(int));
+    
+    *returnSize = 0;
+    int index = 0;
+    
+    helper(nums, numsSize, returnSize, returnColumnSizes, &ret, temp, table, index);
+    
+    return ret;
+}
