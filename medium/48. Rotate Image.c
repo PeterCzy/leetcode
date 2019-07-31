c735dab4dcde30588ae8889891f032c57a908545
PeterCzy <31866285+PeterCzy@users.noreply.github.com>
2019-07-30 21:22:58 -0400

    Create 48. Rotate Image.c

---
+++ medium/48. Rotate Image.c
@@ -0,0 +1,14 @@
+void rotate(int** matrix, int matrixSize, int* matrixColSize){
+    int temp;
+    *matrixColSize = matrixSize;
+    for(int i = 0; i < matrixSize/2; i++){
+        for(int j = i; j < matrixSize - 1 - i; j++){
+            temp = matrix[i][j];
+            
+            matrix[i][j] = matrix[matrixSize - 1 - j][i];
+            matrix[matrixSize - 1 - j][i] = matrix[matrixSize - 1 - i][matrixSize - 1 - j];
+            matrix[matrixSize - 1 - i][matrixSize - 1 - j] = matrix[j][matrixSize - 1 - i];
+            matrix[j][matrixSize - 1 - i] = temp;
+        }
+    }
+}
